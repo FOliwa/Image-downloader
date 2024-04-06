dc77aa7f21b56674dcb9730641079b44e4d05075
FOliwa <filip.oliwa@gmail.com>
2024-04-06 16:03:02 +0200

    Changes in main

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fetcher import ImageFetcherService
 from pdf_creator import PDFCreatorService
+from time import sleep
+from datetime import datetime
 
 # You have to define configs on your own
-from configs import URL
+from configs.cit_conf import LABS, PRESENTATIONS
+from configs.textbooks import BOOKS
+
+
+def get_you_pdf(url, is_textbook=False):
+    print(f'[LOG] {datetime.now().strftime("%Y-%m-%d %H:%M")} - Strat for {url}')
+
+    service = ImageFetcherService(url, is_textbook)
+    imgs_path, pdf_path = service.start()
+    PDFCreatorService().create_pdf_from_png_images(pdf_path, imgs_path)
+
+    print(f'[LOG] {datetime.now().strftime("%Y-%m-%d %H:%M")} END for {url}\n')
+    sleep(4)
 
 
 if __name__ == "__main__":
-    service = ImageFetcherService(URL, is_textbook=False)
-    imgs_path, pdf_path = service.start()
-    PDFCreatorService().create_pdf_from_png_images(pdf_path, imgs_path)
+    for url in BOOKS:
+        get_you_pdf(url, is_textbook=True)
